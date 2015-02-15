0fc0645d1a3426e245ae98fe456dfedd41dc9eb7
Maurice de Beijer <mauricedb@computer.org>
2015-02-15 21:11:35 +0000

    What bower task to use?

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,16 @@
 var gulp = require('gulp');
-var bower = require('gulp-bower2');
+var bower = require('gulp-bower');
+var mainBowerFiles = require('main-bower-files');
 
-gulp.task('bower', function() {
-    return bower()
+//gulp.task('bower', function() {
+//    return bower()
+//        .pipe(gulp.dest('wwwroot/lib/'))
+//});
+
+
+gulp.task('mainBowerFiles', function() {
+    return gulp.src(mainBowerFiles())
         .pipe(gulp.dest('wwwroot/lib/'))
 });
 
-
-gulp.task('default',['bower']);
+gulp.task('default',['mainBowerFiles']);
