25a2572cf5cd2a11c7930cae24100e9f1a45cde9
Maurice de Beijer <mauricedb@computer.org>
2015-03-15 10:13:43 +0100

    Fixed ES6 loader dependency

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var mainBowerFiles = require('main-bower-files');
 gulp.task('jsx', function () {
     return gulp.src('app/**/*.jsx')
         .pipe(plugins.babel({
-            modules :'system'
+            modules: 'system'
         }))
         .pipe(plugins.rename({
             suffix: '.jsx',
@@ -17,15 +17,19 @@ gulp.task('jsx', function () {
 gulp.task('js', function () {
     return gulp.src('app/**/*.js')
         .pipe(plugins.babel({
-            modules :'system'
+            modules: 'system'
         }))
         .pipe(gulp.dest('wwwroot/app'));
 });
 
 gulp.task('mainBowerFiles', function () {
-    return gulp.src(mainBowerFiles(),
-        {base: './bower_components'})
-        .pipe(gulp.dest('wwwroot/lib/'))
+
+    gulp.src('./node_modules/es6-module-loader/dist/es6-module-loader.js')
+        .pipe(gulp.dest('wwwroot/lib/system.js/dist/'));
+
+    return gulp.src(mainBowerFiles(), {
+        base: './bower_components'
+    }).pipe(gulp.dest('wwwroot/lib/'))
 });
 
 gulp.task('watch', function () {
