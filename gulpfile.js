08fd3358a084b3fdcdac671eb72c01a721b97423
Maurice de Beijer <mauricedb@computer.org>
2015-03-16 19:01:42 +0100

    Clean out system.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,9 @@
 var gulp = require('gulp');
 var plugins = require('gulp-load-plugins')();
-var mainBowerFiles = require('main-bower-files');
 
 gulp.task('jsx', function () {
     return gulp.src('app/**/*.jsx')
-        .pipe(plugins.babel({
-            modules: 'system'
-        }))
+        .pipe(plugins.babel())
         .pipe(plugins.rename({
             suffix: '.jsx',
             extname: '.js'
@@ -14,27 +11,9 @@ gulp.task('jsx', function () {
         .pipe(gulp.dest('wwwroot/app'));
 });
 
-gulp.task('js', function () {
-    return gulp.src('app/**/*.js')
-        .pipe(plugins.babel({
-            modules: 'system'
-        }))
-        .pipe(gulp.dest('wwwroot/app'));
-});
-
-gulp.task('mainBowerFiles', function () {
-
-    gulp.src('./node_modules/es6-module-loader/dist/es6-module-loader.js')
-        .pipe(gulp.dest('wwwroot/lib/system.js/dist/'));
-
-    return gulp.src(mainBowerFiles(), {
-        base: './bower_components'
-    }).pipe(gulp.dest('wwwroot/lib/'))
-});
-
 gulp.task('watch', function () {
-    gulp.watch('app/**/*.js', ['js']);
+    gulp.watch('app/**/*.js', ['jsx']);
     gulp.watch('app/**/*.jsx', ['jsx']);
 });
 
-gulp.task('default', ['mainBowerFiles', 'jsx', 'js', 'watch']);
+gulp.task('default', ['jsx', 'watch']);
